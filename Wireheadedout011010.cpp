99b1594a052cd5a343cbc646107a3d7791129765
nuLL <85875375+nul10@users.noreply.github.com>
2022-07-07 22:36:38 +0600

    Add files via upload

--- Wireheadedout011010.cpp
+++ Wireheadedout011010.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!";
+    int x;
+    int y;
+    int total;
+
+    cout << "enter your social security number \n";
+    cin >> x;
+
+    cout << "enter those funky numbers on the back of your parents credit card \n";
+    cin >> y;
+    total = x + y;
+    cout << "the total of those numbers are " << total << endl;
+    cout << "have a nice day! ( @ ^ v ^)";
+    
+    return 0;
 }
